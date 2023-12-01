b1f389ec4a11419cf415262115ee502601f0eefc
Worthy <worthyintech@gmail.com>
2023-12-01 15:35:30 +0000

    Working on the parseInput() functionality.

--- SimpleShell/parseInput.cpp
+++ SimpleShell/parseInput.cpp
@@ -7,13 +7,26 @@
 
 #include "parseInput.hpp"
 #include <string>
+#include <iostream>
 
 
 std::vector<std::string> parseInput(std::string userInput) {
     std::vector<std::string> commands {};
     
-    commands.push_back(userInput);
-    commands.push_back(userInput);
+    std::string command {};
+    
+    // Iterating through the user input and splitting when a space is found.
+    for (char c : userInput) {
+        if (c != ' ')
+            command += c;
+        else {
+            commands.push_back(command);
+            command = "";
+        }
+    }
+    
+    // Need to make another push to account for the last command that is followed by a space.
+    commands.push_back(command);
     
     return commands;
 }
