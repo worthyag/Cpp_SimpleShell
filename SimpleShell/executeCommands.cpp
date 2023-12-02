ea29eaab799579cf5597657fe136e81f629ac7be
Worthy <worthyintech@gmail.com>
2023-12-02 19:49:43 +0000

    Working on the functionality for executeCommands() function.

--- SimpleShell/executeCommands.cpp
+++ SimpleShell/executeCommands.cpp
@@ -7,10 +7,40 @@
 
 #include "executeCommands.hpp"
 #include <iostream>
+#include <vector>
 
 void executeCommands(std::vector<std::string> tokens) {
-    for (std::string word : tokens) {
-        std::cout << word << " | ";
+//    for (std::string word : tokens) {
+//        std::cout << word << " | ";
+//    }
+//    std::cout << std::endl;
+    
+    auto echo {
+        [&tokens] () {
+            tokens.erase(tokens.begin());
+            
+            std::string toPrint {};
+            
+            for (const std::string& word : tokens) {
+                toPrint += word;
+                toPrint += " ";
+            }
+            
+            std::cout << toPrint << std::endl;
+            
+        }
+    };
+    
+    if (tokens[0] == "echo") {
+        echo();
     }
-    std::cout << std::endl;
+    
+//    switch (tokens[0]) {
+//        case <#constant#>:
+//            <#statements#>
+//            break;
+//            
+//        default:
+//            break;
+//    }
 }
